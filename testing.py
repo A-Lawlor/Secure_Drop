eecbeda55a90ddb13c4cf85e7fb3ba5902b896c5
Anthony <78994801+A-Lawlor@users.noreply.github.com>
2021-12-08 19:41:47 -0500

    Added support to transfer files

--- testing.py
+++
@@ -1,9 +0,0 @@
-import cryptocode
-
-password = "hey"
-message = "Testing123"
-
-encrypted = cryptocode.encrypt(message, password)
-print(encrypted)
-decrypted = cryptocode.decrypt(encrypted, password)
-print(decrypted)
